27be8ee839569d734549ec9914038449e12bda05
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-12-21 22:12:26 +0300

    edit menu.js

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,21 +1,24 @@
-function Menu() {
-
+function Menu(props) {
     const items = [
         { title: 'Главная страница', href: '#home' },
         { title: 'О компании', href: '#about' },
         { title: 'Контакты', href: '#contact' }
-    ];
+    ];        
 
-    const opened = true;
-    
+    if (props.opened) {
+        return (
+            <div className="menu menu-open">
+                <div className="menu-toggle"><span></span></div>
+                <nav>
+                <ul>{items.map(obj => <li><a href={obj.href} key={obj.title}>{obj.title}</a></li>)}</ul>
+                </nav>
+            </div>
+        );
+    }
+
     return (
-        <div className="menu menu-open">
-            <div className="menu-toggle"><span></span></div>
-            <nav>
-            <ul>{items.map(obj => <li><a href={obj.href} key={obj.title}>{obj.title}</a></li>)}</ul>
-            </nav>
-        </div>        
-    );
+        <div className="menu">
+            <div className="menu-toggle"><span></span></div>                
+        </div> 
+    )        
 }
-
-// export default Menu;
